24b603388a2bcc99cab87114f41e21ef77ac33c0
Riley DuBord <thelifeofdu@gmail.com>
2024-05-28 18:02:32 -0500

    App.js now has protected routes, needs a whole lot of testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-// Library imports
+// src/App.js
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-
-// File imports
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Frontend/Header';
 import TaskGrid from './Frontend/TaskGrid';
 import AddTaskButton from './Frontend/AddTaskButton';
 import theme from './Styles/theme';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
-      <TaskGrid />
+      <Router>
+        <AuthProvider>
+          <Header />
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <TaskGrid />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </ChakraProvider>
   );
 }
