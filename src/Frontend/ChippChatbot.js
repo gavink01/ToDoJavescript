5f6550e3bcb8e43be71b8ea370bc0ed92da66bb5
Riley DuBord <thelifeofdu@gmail.com>
2024-05-28 10:49:23 -0500

    chatbot styling finished

--- src/Frontend/ChippChatbot.js
+++ src/Frontend/ChippChatbot.js
@@ -8,27 +8,29 @@ const ChatbotModal = () => {
     <Box position="fixed" bottom={4} right={4}>
       <Button onClick={onOpen} colorScheme="accent" size={'lg'} width={'300px'}>Chipp.AI Chatbot </Button>
 
-      <Modal isOpen={isOpen} onClose={onClose} size="full">
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent
+          maxW="700px"
+          maxH="700px"
+          ml="10px"
+          mt="10px" 
+          borderRadius="10px" 
+          boxShadow="lg"
+
+        >
           <ModalHeader>Chipp.AI Chatbot</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <iframe 
-              src="https://kirkwall-6420.chipp.ai" 
-              height="800px" 
-              width="100%" 
-              frameborder="0" 
-              title="Chipp.AI Chatbot"
-              style={{ width: '100%', height: 'calc(100vh - 100px)', border: 'none' }} 
+          <iframe
+              src="https://kirkwall-6420.chipp.ai"
+              height="600px"
+              width="100%"
+              frameborder="0"
+              title="Kirkwall"
+              style={{ border: "none" }}
             />
           </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
       </Box>
